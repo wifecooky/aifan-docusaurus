2318e134c6423716c06ecdbeada5b446390058f5
wen <wifecooky@gmail.com>
2023-10-28 01:10:37 +0900

    SEO: metadata

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,6 +72,8 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/social-card.png',
+      // Metadatas for your site
+      metadata: [{ name: 'keywords', content: 'programming, tokyo, hangzhou' }],
       navbar: {
         title: 'thewang',
         logo: {
